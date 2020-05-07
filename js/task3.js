9ba691aca7c3f5c50ea1928e4a82e022cb638e10
Maximus36 <vater_hill@mail.ru>
2020-05-07 14:22:07 +0300

    added homework

---
+++ js/task3.js
@@ -0,0 +1,19 @@
+const findLongestWord = function(string) {
+	const array = string.split(' ');
+	let longestWord = '';
+	for (let i = 0; i < array.length; i++) {
+		if (array[i].length > longestWord.length) {
+			longestWord = array[i];
+		}
+	}
+	return longestWord;
+};
+
+/*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
